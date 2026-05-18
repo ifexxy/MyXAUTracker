304e19073bc64537fec65ca8fe48bf85e6a36012
ifexxy <68091604+ifexxy@users.noreply.github.com>
2026-05-18 12:32:04 +0100

    Add Firebase configuration file

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const fbConfig = {
+  apiKey:            "AIzaSyCVUXlnWPqtVQnqxHNyABktkFxnRj3mFvs",
+  authDomain:        "xautracker.firebaseapp.com",
+  projectId:         "xautracker",
+  storageBucket:     "xautracker.firebasestorage.app",
+  messagingSenderId: "831752609455",
+  appId:             "1:831752609455:web:ea9be478691744afa73e5a"
+};
