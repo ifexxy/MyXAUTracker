eed5ad557128a96e9a270d4124e6e8cbbd27800b
ifexxy <68091604+ifexxy@users.noreply.github.com>
2026-05-26 20:07:25 +0100

    Add files via upload

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(clients.claim()));
+self.addEventListener('notificationclick', (e) => {
+  e.notification.close();
+  e.waitUntil(clients.matchAll({ type: 'window', includeUncontrolled: true }).then(cls => {
+    if (cls[0]) { cls[0].focus(); return; }
+    clients.openWindow('/');
+  }));
+});
